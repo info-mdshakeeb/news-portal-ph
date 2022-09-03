44218b73dfb71ff799cfb77fe6df8170136cbc00
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-09-03 13:34:00 +0600

    modal patr added

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ const setManue = async () => {
     });
 }
 setManue();
-
 const loadNews = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/news/category/01');
     const data = await response.json();
@@ -25,23 +24,56 @@ const displayNews = async () =>{
     const data = await loadNews();
     const newsDiv = document.getElementById('card-container');
     data.data.forEach(element =>{
-        console.log(element)
+        const {title,details,author,total_view,image_url} = element;
     const createDiv =document.createElement('div');
     createDiv.classList.add('card','my-3')
     createDiv.innerHTML = `
-    <div class="row ">
-    <div class="col-md-4">
-      <img class="img-fluid rounded-start" alt="..." src="${element.image_url}" />
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${element.title}</h5>
-        <p class="card-text">${element.details.slice(0,500)}...</p>
-      </div>
-    </div>
-  </div>`
+    <div class="row">
+            <div class="col-md-4">
+              <img
+                class="img-fluid rounded-start"
+                alt="..."
+                src="${image_url}"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${details.length > 200 ? details.slice(0,200) + "..." :details}</p>
+                <div class="row">
+                  <div class="col-1">
+                    <img class="img-fluid rounded-circle" alt="No image" src="${author.img}" />
+                  </div>
+                  <div class="col-md-4 ">
+                  <div class="">${author.name}</div>
+                  <div class="">${author.published_date}</div></div>
+                  <div class="col-md-4">
+                  <i class="fa-solid fa-eye"></i>
+                  ${total_view}</div>
+                  <div class="col-md-3">
+                  <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="readFullMessage('${author.img}','${author.name}','${author.published_date}')">Author Detail</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`
     newsDiv.appendChild(createDiv);
     })
-}
+};
+const readFullMessage = (img, name, date) =>{
+    const moduleDiv = document.getElementById('module');
+    const createDiv = document.createElement('div');
+    moduleDiv.textContent = '';
+    createDiv.innerHTML =`
+                <div class="card border-0">
+                  <img src="${img}" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">"${name}"</h5>
+                    <p class="card-text">
+                     "${date}"
+                    </p>
+                  </div>
+                </div>`
+    moduleDiv.appendChild(createDiv);
+  }
 displayNews();
-
